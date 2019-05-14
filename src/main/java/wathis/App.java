a7cf4b411602d5569281d395d317fbf15d937bf7
Thomas <herbelin.thomas@gmail.com>
2019-05-14 16:34:30 +0200

    La vérité est rétablie

--- src/main/java/wathis/App.java
+++ src/main/java/wathis/App.java
@@ -4,10 +4,10 @@ package wathis;
  * Hello world!
  *
  */
-public class App 
+public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+        System.out.println( "Hello World! Gael c'est un beau gosse <3" );
     }
 }
