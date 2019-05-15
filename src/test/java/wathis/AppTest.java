3325d5460f4d831e5a8c82d03a1f5191617e5c54
Wathis <delaunaymathis@yahoo.fr>
2019-05-15 14:55:40 +0200

    test

--- src/test/java/wathis/AppTest.java
+++ src/test/java/wathis/AppTest.java
@@ -4,34 +4,44 @@ import junit.framework.Test;
 import junit.framework.TestCase;
 import junit.framework.TestSuite;
 
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+
 /**
  * Unit test for simple App.
  */
 public class AppTest 
-    extends TestCase
-{
+    extends TestCase {
     /**
      * Create the test case
      *
      * @param testName name of the test case
      */
-    public AppTest( String testName )
-    {
-        super( testName );
+    public AppTest(String testName) {
+        super(testName);
     }
 
     /**
      * @return the suite of tests being tested
      */
-    public static Test suite()
-    {
-        return new TestSuite( AppTest.class );
+    public static Test suite() {
+        return new TestSuite(AppTest.class);
     }
 
-    /**
-     * Rigourous Test :-)
-     */
+    private final ByteArrayOutputStream outContent = new ByteArrayOutputStream();
+    private final PrintStream originalOut = System.out;
+
+    public void testHelloWorld() {
+        String helloWorld = "Hello World! ©Florian Le Menn  2019";
+        System.setOut(new PrintStream(outContent));
+        System.out.print(helloWorld);
+        assertEquals(helloWorld, outContent.toString());
+        System.setOut(originalOut);
+    }
+
     public void testApp() {
-        App.main(null);
+
+        App app = new App();
+        assertNotNull(app);
     }
 }
